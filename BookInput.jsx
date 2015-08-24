89ca0982940a4e9125151f9fc1a07977dbba2967
konojunya <konojunya@konojunya-no-MacBook-Air.local>
2015-08-24 14:11:17 +0900

    empty file

--- BookInput.jsx
+++ BookInput.jsx
@@ -2,11 +2,31 @@ BookInput = React.createClass({
   getInitialState: function() {
    return {value: 'Hello!'};
  },
- handleChange: function(event) {
+ _handleChange: function(event) {
     this.setState({value: event.target.value.substr(0, 140)});
     console.log(event);
   },
+ _handleSubmit: function(event) {
+   Books.insert({title: this.state.value})
+  },
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    return {
+      allBooks: Books.find().fetch()
+    };
+  },
  render: function() {
-   return <input type="text" value={this.state.value} onChange={this.handleChange} />;
+   return (
+     <div>
+       <h1>{this.state.value}</h1>
+      <p>
+        {this.data.allBooks.map(function(book) {
+            return <p>{book.title}</p>;
+          })}
+      </p>
+       <input type="text" value={this.state.value} onChange={this._handleChange} />
+       <input type="submit" value="登録" onClick={this._handleSubmit} />
+     </div>
+   );
  }
 });
