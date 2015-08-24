9d513a871d47efaf33cd35cd24c530bda3de7721
konojunya <konojunya@konojunya-no-MacBook-Air.local>
2015-08-24 14:46:40 +0900

    h1のcolorのプログラム

--- BookInput.jsx
+++ BookInput.jsx
@@ -1,13 +1,20 @@
 BookInput = React.createClass({
   getInitialState: function() {
-   return {value: 'Hello!'};
+   return {
+     value: 'Hello!',
+     color_name: 'red'
+   };
  },
  _handleChange: function(event) {
     this.setState({value: event.target.value.substr(0, 140)});
-    console.log(event);
   },
  _handleSubmit: function(event) {
    Books.insert({title: this.state.value})
+  },
+  _color: function(event){
+    var color_array =['red','blue','yellow','green']
+    var rand = Math.floor( Math.random() * 4)
+    this.setState({color_name: color_array[rand]})
   },
   mixins: [ReactMeteorData],
   getMeteorData() {
@@ -16,9 +23,12 @@ BookInput = React.createClass({
     };
   },
  render: function() {
+   var divStyle = {
+      color: this.state.color_name
+    };
    return (
      <div>
-       <h1>{this.state.value}</h1>
+       <h1 style={divStyle}>{this.state.value}</h1>
       <p>
         {this.data.allBooks.map(function(book) {
             return <p>{book.title}</p>;
@@ -26,6 +36,7 @@ BookInput = React.createClass({
       </p>
        <input type="text" value={this.state.value} onChange={this._handleChange} />
        <input type="submit" value="登録" onClick={this._handleSubmit} />
+       <input type="submit" value="color" onClick={this._color} />
      </div>
    );
  }
