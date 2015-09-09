20aa0934c855523c7483e3b6c014b5eb2d30c734
konojunya <junya03dance@gmail.com>
2015-09-09 23:08:33 +0900

    close #21

--- collections/Book.js
+++ collections/Book.js
@@ -1,6 +1,18 @@
 Books = new Mongo.Collection("books");
 Tickets = new Mongo.Collection("tickets");
 
+Books.allow({
+  insert: function(){
+    return Meteor.userId()?true:false;
+  },
+  update: function(){
+    return Meteor.userId()?true:false;
+  },
+  remove: function(){
+    return Meteor.userId()?true:false;
+  }
+});
+
 if(Meteor.isServer){
   Meteor.methods({
     removeBook: function (bookId) {
